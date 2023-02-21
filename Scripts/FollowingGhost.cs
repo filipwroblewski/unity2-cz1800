3980ff0c793732c0d9e958af12c40dbc6c9924a1
Filip Wróblewski <75478989+filipwroblewski@users.noreply.github.com>
2023-02-21 18:45:50 +0100

    Add files via upload

--- Scripts/FollowingGhost.cs
+++ Scripts/FollowingGhost.cs
@@ -7,6 +7,11 @@ public class FollowingGhost : MonoBehaviour
     private Transform player;
     private Transform enemy;
 
+    private float turnSpeed = 5.0f; // predkosc obrotu Enemy
+    private float movementSpeed = 5.0f; // predkosc poruszania sie Enemy
+    private float range = 25.0f; // zasieg widzenai Enemy
+    private float closestRange = 2.0f; // jak blisko moze podesjc Enemy do Player
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +23,14 @@ public class FollowingGhost : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // obliczenie dystansu od gracza
+        float distance = Vector3.Distance(enemy.position, player.position);
+
+        // jezeli Player bedzie w zasiegu wzorku Enemy i Enemy nie jest za blisko Player
+        if (true)
+        {
+            Debug.Log("Teraz ide");
+        }
         
     }
 }
