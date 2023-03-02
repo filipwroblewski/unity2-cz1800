5ddd00aa241c08dcdd97d4290a39f2cb0e13ae7b
Filip Wróblewski <75478989+filipwroblewski@users.noreply.github.com>
2023-03-02 18:13:14 +0100

    Add files via upload

--- Scripts/FollowingGhost.cs
+++ Scripts/FollowingGhost.cs
@@ -26,22 +26,22 @@ public class FollowingGhost : MonoBehaviour
         // obliczenie dystansu od gracza
         float distance = Vector3.Distance(enemy.position, player.position);
 
-
-
         // v1
-        if (true) // jezeli Player bedzie w zasiegu wzorku Enemy i Enemy nie jest za blisko Player
+        if (distance < range && distance > closestRange) // jezeli Player bedzie w zasiegu wzorku Enemy i Enemy nie jest za blisko Player
         {
-            Debug.Log("Teraz ide");
+            enemy.rotation = Quaternion.Slerp(enemy.rotation, Quaternion.LookRotation(player.position - enemy.position), 4.0f * Time.deltaTime);
+            enemy.position += enemy.forward * 2.0f * Time.deltaTime; 
         }
-
+        
+        
         // v2
-        if (true) // jezeli Player bedzie w zasiegu wzorku Enemy
+        if (distance < range) // jezeli Player bedzie w zasiegu wzorku Enemy
         {
-            if (true) // Enemy nie jest za blisko Player
+            if (distance > closestRange) // Enemy nie jest za blisko Player
             {
-                Debug.Log("Teraz ide");
+                enemy.rotation = Quaternion.Slerp(enemy.rotation, Quaternion.LookRotation(player.position - enemy.position), 4.0f * Time.deltaTime);
+                enemy.position += enemy.forward * 2.0f * Time.deltaTime;
             }
         }
-        // ------
     }
 }
