9ad4a8cdeb304227f3f45bb35f99577a394e8b2b
Filip Wróblewski <75478989+filipwroblewski@users.noreply.github.com>
2023-03-16 18:38:36 +0100

    Add files via upload

--- Scripts/FollowingWithCollisions.cs
+++ Scripts/FollowingWithCollisions.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class FollowingWithCollisions : MonoBehaviour
 {
-    private float range = 25.0f; // zasieg widzenai Enemy
-    private float closestRange = 2.0f; // jak blisko moze podesjc Enemy do Player
+    float range = 25.0f; // zasieg widzenai Enemy
+    float closestRange = 2.0f; // jak blisko moze podesjc Enemy do Player
 
     Transform enemy; // zmienna Enemy Transform
     CharacterController enemyController; // zmienna Enemy Character Controller
     Transform player; // zmienna Player Transform
+
+    float turnSpeed = 4.0f; // predkosc obrotu
+    float currentJumpHight = 0.0f; // aktualna wysokosc skoku
 
     // Start is called before the first frame update
     void Start()
@@ -27,20 +30,19 @@ public class FollowingWithCollisions : MonoBehaviour
         // float, Vector3.Distance(pozycjaEnemy, pozycjaPlayer)
         float distance = Vector3.Distance(enemy.position, player.position);
 
-        if (distance < range && distance > closestRange) // jezeli Player bedzie w zasiegu wzorku Enemy i Enemy nie jest za blisko Player
+        // jezeli Player bedzie w zasiegu wzorku Enemy i Enemy nie jest za blisko Player
+        if (distance < range && distance > closestRange) 
         {
             Vector3 playerPosition = new Vector3(player.position.x, enemy.position.y, player.position.z);
-            enemy.rotation = Quaternion.Slerp(enemy.rotation, Quaternion.LookRotation(player.position - enemy.position), 4.0f * Time.deltaTime);
+            enemy.rotation = Quaternion.Slerp(enemy.rotation, Quaternion.LookRotation(player.position - enemy.position), turnSpeed * Time.deltaTime);
         }
 
         // sprawdzenie czy postac jest na ziemi
-
-        // !enemyController.isGrounded
-        // enemyController.isGrounded == false
         if (!enemyController.isGrounded)
         {
-            // cos zrob
-            Debug.Log("");
+            // jestem w powietrzu wiec -> 
+            Debug.Log("cos zrob");
+            //currentJumpHight
         }
         
 
