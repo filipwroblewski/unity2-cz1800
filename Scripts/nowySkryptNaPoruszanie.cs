573fd9be33a8d9ff6c810f1cba855f6bee6a1e3b
Filip Wróblewski <75478989+filipwroblewski@users.noreply.github.com>
2023-04-13 19:09:22 +0200

    Add files via upload

---
+++ Scripts/nowySkryptNaPoruszanie.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class nowySkryptNaPoruszanie : MonoBehaviour
+{
+    [SerializeField] float turnSpeed = 4.0f;
+    [SerializeField] float movementSpeed = 2.0f;
+    [SerializeField] float closestRange = 2.0f;
+    [SerializeField] float range = 30.0f;
+
+    Transform enemy;
+    Transform player;
+
+    Vector3 playerXYZ;
+
+    Rigidbody rgbd;
+
+    bool lookAtPlayer;
+    [SerializeField] bool smoothTurn = true;
+
+    void Start()
+    {
+        enemy = transform;
+        rgbd = GetComponent<Rigidbody>();
+
+        if (rgbd)
+        {
+            rgbd.freezeRotation = true;
+        }
+    }
+
+    void Update()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerXYZ = new Vector3(player.position.x, enemy.position.y, player.position.z);
+
+        lookAtPlayer = false;
+
+        float distance = Vector3.Distance(enemy.position, playerXYZ);
+        if (distance <= range && distance > closestRange)
+        {
+            lookAtPlayer = true;
+            enemy.position = Vector3.MoveTowards(enemy.position, playerXYZ, movementSpeed * Time.deltaTime);
+        }else if (distance <= closestRange)
+        {
+            lookAtPlayer = true;
+        }
+
+        lookAtMe();
+    }
+
+    void lookAtMe()
+    {
+        if(lookAtPlayer == true && !smoothTurn)
+        {
+            enemy.LookAt(playerXYZ);
+        }else if(lookAtPlayer == true && smoothTurn)
+        {
+            Quaternion rotation = Quaternion.LookRotation(player.position - enemy.position);
+            enemy.rotation = Quaternion.Slerp(enemy.rotation, rotation, turnSpeed * Time.deltaTime);
+        }
+    }
+}
